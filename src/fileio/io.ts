9393f6fbf6d1c7753df9c71666842db6271863c9
Markus Ryöti <markus.ryoti@student.tut.fi>
2021-01-10 21:55:49 +0200

    changed io function, file will be created if doesn't exist

--- src/fileio/io.ts
+++ src/fileio/io.ts
@@ -22,6 +22,10 @@ export const writeMarkdownToFileWithPath = async (
 };
 
 export const readMarkdownFromFile = async (): Promise<string | void> => {
+  if (!fs.existsSync(fileName)) {
+    fs.writeFile(fileName, '', () => {});
+  }
+
   const contentPromise: Promise<string | void> = new Promise(
     (resolve, reject) => {
       fs.readFile(
